324ff21e5ca402fb5b190d882b856542bba0262a
shyyykitty <none>
2024-06-05 22:04:28 -0400

    first working version

--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
-all: build/out.bin
+all: build/rom.h
 
 build/%.o : %.asm
 	z88dk-z80asm -mz80 -o$@ $< 
 
-build/out.bin: build/main.o build/util.o
-	z88dk-z80asm -b -obuild/out.bin build/*.o
+build/rom.bin: build/main.o build/util.o
+	z88dk-z80asm -b -obuild/rom.bin build/*.o
+
+build/rom.h: build/rom.bin
+	xxd -i build/rom.bin > build/rom.h
+	sed -i s/build_rom_bin/Rom/g build/rom.h
 
 clean:
 	rm -r build
--- main.asm
+++ main.asm
@@ -1,21 +1,21 @@
 ORG 0
 
 .main
-	LD A, (0xAAAA)
-	LD (0x5555), A
+	LD A, (0x00F0)
+	LD A, (0x000F)
 	JP main
 
-DS 0x38 - $, 0
+;DS 0x38 - $, 0
 
-ASSERT $ == 0x38, "Main subroutine must be <= 56 bytes"
+;ASSERT $ == 0x38
 
-.interrupt_handler
-	call some_subroutine
-	EI
-	RETI
-
-.some_subroutine
-
-	ADD A, 12
-	RET
+;.interrupt_handler
+;	call some_subroutine
+;	EI
+;	RETI
+;
+;.some_subroutine
+;
+;	ADD A, 12
+;	RET
 
